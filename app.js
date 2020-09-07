ccb4f0dc90669714a9966e8c5b7ecd48bab422f6
kinivasu <vasudevkinit2001@gmail.com>
2020-09-07 16:26:37 +0530

    modified app

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 //const sessions = require("express-sessions");
-const morgan = require('morgan');
+const morgan = require("morgan");
 
-//express app
 const app = express();
 
-//connect to the db
+//static files inside the public folder
+app.use(express.static("public"));
+//view engine
+app.set("view engine", "ejs");
+
+//connect to the db , env variables set in the .env file
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -17,15 +21,22 @@ const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Connected to DB"));
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
-//routes
-// const loginRouter = require("./routes/login");
-// const morgan = require("morgan");
-// app.use("/login", loginRouter);
-
-//listen at port 3000
+//listening at port 3000
 app.listen(3000, () => {
-  console.log("Server has started at localhost:3000")
+  console.log("Server has started at localhost:3000");
 });
+
+// routes
+app.get("/", (req, res) => res.render("index"));
+app.get("/login", (req, res) => res.render("login"));
+app.get("/signup", (req, res) => res.render("signup"));
+// app.post
+// app.get()
+// app.get('/user')
+// app.get('/admin')
+
+//last middleware for 404 error
+app.use((req, res) => res.status(404).render("404"));
--- readme.md
+++ readme.md
@@ -1,2 +1,19 @@
-## Readme for this project
-#### Good Readme Practices
+<!--* NOTE! -->
+<!--* Use Ctrl+K V to preview the markdown file in VS code-->
+<!--* How to write an effective markdown: https://medium.com/echohub/write-simple-and-effective-markdown-tips-8e01fdddd70 -->
+
+# SysAdmin Task-1 Portal
+
+### Test Users
+1. email : test@gmail.com 
+pass : password 
+type : NON-ADMIN (BASIC)
+
+
+2. email : admin@gmail.com 
+pass : password 
+type : ADMIN
+
+***
+
+***
